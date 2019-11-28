a039d7d6a086f8e9fb88b5b4775bc0cd4023dc05
root <root@security2u.com>
2019-11-28 17:59:59 +0530

    python ver.1

---
+++ pythonfolder.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+country="India"
+
+if country == "AUS":
+
+  print("1.not a valid country")
+
+elif country == "USA":
+  print("2.not a valid country")
+
+elif country == "CHN":
+
+ print("3.not a valid country")
+
+elif country =="India":
+
+  print("4. Country name found : India")
+
+else:
+
+ print("not in list")
