1728baad05a678228d705133247824225cc761c6
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2020-02-29 00:04:18 +0530

    Add files via upload
    Split method

---
+++ split method.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <----------------"Split method"------------------------->
+
+# In[44]:
+
+
+x='john,wick,wild,gnome'
+
+
+# In[45]:
+
+
+x.split()
+
+
+# In[47]:
+
+
+x.split(",")
+
+
+# In[52]:
+
+
+y='a     b c'
+
+
+# In[53]:
+
+
+y.split()
+
+
+# In[54]:
+
+
+y.split(' ')
+
+
+# In[ ]:
+
+
+
+
