9b9ddb1b860adf5a6c208b3cdafded6ba3135224
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-04-16 23:43:47 +0530

    function examples

--- covid_2.py
+++ covid_2.py
@@ -1,5 +1,13 @@
 #!/bin/python3
 
 
+def oldProgram(code):
+    print("Welcome to you, ",write)
+    pro = input("Enter your choice : ")
+    if pro == "":
+        print("At least fill this column.")
+    else:
+        print(pro + " is a good choice.")
 
-def 
+write = input("Write your name : ")
+oldProgram(write)
