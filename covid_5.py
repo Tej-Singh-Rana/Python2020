48e056da8df70e682eaeba99949ee48fa611e02d
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-04-17 19:19:03 +0530

    while loop & else statement

--- covid_5.py
+++ covid_5.py
@@ -1,7 +1,16 @@
 #!/bin/python3
 
 
+language = ['Python', 'C++', 'Go', 'C#', 'Java']
+initial = 0
 
+while initial < len(language):
+    print(str(initial) + " " + language[initial])
+    if language[initial] == 'C#':
+        print("Found this language in position : ",initial)
+        break
+    initial += 1
 
-
-
+# else statement will run when no object found in while loop.
+else:
+    print("Not found in list.")
