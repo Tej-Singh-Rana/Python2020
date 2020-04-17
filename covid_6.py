43429ba2350202c4804b1098d76ecf04655bc26e
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-04-18 02:53:22 +0530

    function & if-elif-else statement

--- covid_6.py
+++ covid_6.py
@@ -1,1 +1,25 @@
 #!/bin/python3
+
+import os
+
+def func():
+  x = input("enter python extention : ")
+  if x == "":
+    print("invalid entry.")
+  elif x == '.py' or x == 'py':
+    print("Valid extention!!")
+  elif x.isdigit():
+    print("extention must be alphabet.")
+  else:
+    print('enter correct extention to verified.')
+  return x
+func()
+
+if func() == 'py' or func() == '.py':
+  print("Don't need to add extention.")
+  file = input("Enter file name to create : ")
+  os.system('touch {}.py'.format(file))
+  print(file +".py file created.")
+else:
+  print("Cannot create file.")
+
