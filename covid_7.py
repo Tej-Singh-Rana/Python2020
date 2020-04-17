b91131fe90f9eaf1963175e994c3e4ac49630f38
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-04-18 02:54:26 +0530

    dict examples

--- covid_7.py
+++ covid_7.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 
 
+status = ['code', 'program', 'script', 'accuracy', 'function', 'static', 'concurrency', 'fault tolerance']
+match = ['error', 'index', 'method', 'stack', 'chunk', 'function', 'handler', 'bugs']
 
-
-
-
+for coke in status:
+  if coke not in match:
+    print("List of items not present in match category :-- ",coke)
