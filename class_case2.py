3f23dc170e12e1a3b72d4c02f201469508125204
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-05-02 23:30:58 +0530

    Instance method example

--- class_case2.py
+++ class_case2.py
@@ -1,4 +1,21 @@
 
+import math    # importing math module
+# instance method
 
+class Circle():
 
-class
+    # class attribute
+    is_shape = True
+
+    def __init__(self, radius, color='red'):
+        # instance attribute
+        self.radius = radius
+        self.color = color
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+# declaration 
+circle = Circle(float(input()))
+x = circle.area()
+print(x)
