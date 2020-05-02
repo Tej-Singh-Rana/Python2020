66d651bb467fa7ae23c49987f8eea6100662b9c3
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-05-02 06:28:01 +0530

    example of classes

--- class_case1.py
+++ class_case1.py
@@ -1,5 +1,16 @@
 
 
+class Number():
+    count = 10
 
+    def __init__(self, t1, t2):
+        self.t1 = t1
+        self.t2 = t2
 
-class 
+case = Number(input("Number one : "),input("Number two : "))
+x = case.t1
+print(x)
+y = case.t2
+print(y)
+z = case.count
+print(z)
