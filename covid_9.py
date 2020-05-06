a49a2ff75b9655397db95ca187dbf9de3c1d2fc3
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-05-06 23:59:12 +0530

    __str__ method

--- covid_9.py
+++ covid_9.py
@@ -1,8 +1,20 @@
 #!/bin/python3
 
+class User():
 
+	is_value = 50
 
+	def __init__(self, user, data):
+		self.user = user
+		self.data = data
 
+	def __str__(self):
+
+		return 'Hello, Welcome to this world', self.user
+
+r1 = User('Rahul','Raw materials')
+c1 = r1.__str__()
+print(c1)
 
 
 
