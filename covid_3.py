bf97549e002bcae207a2c94ec1df46aa57c72bdf
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-05-15 00:15:54 +0530

    Fixed minor issues

--- covid_3.py
+++ covid_3.py
@@ -9,12 +9,18 @@ class Code():
 		self.task = task
 
 	def main(self):
-		press = int(input("Enter the unicode value [1, 2, 3, 4, 5] :- "))
-
-		if press in x:
-			print("Your code is right.")
-		return press
-
+		try:
+			press = int(input("Enter the unicode value [1, 2, 3, 4, 5] :- "))
+			x = [1, 2, 3, 4, 5]
+			if press in x:
+				return "Your code is right."
+			elif press > 5:
+				return "Enter digit between 1 to 5."
+			else:
+				return "Kindly follow procedures."
+		except Exception as err:
+			return "Enter only digit b/w 1 to 5."
 if __name__ == "__main__":
-	y = Code("unicode")
-	print(y)
+	y = Code("one")
+	z = y.main()
+	print(z)
