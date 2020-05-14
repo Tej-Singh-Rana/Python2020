6a48805c1128cd82f6a0ae5659fbd7eb47e3a5e3
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-05-14 23:59:06 +0530

    Class method

--- covid_3.py
+++ covid_3.py
@@ -1,6 +1,20 @@
 #!/bin/python3
 
 
-class code:
+class Code():
+	x = [1, 2, 3, 4, 5]
 
-    def 
+	def __init__(self, unicode, task=None):
+		self.unicode = unicode
+		self.task = task
+
+	def main(self):
+		press = int(input("Enter the unicode value [1, 2, 3, 4, 5] :- "))
+
+		if press in x:
+			print("Your code is right.")
+		return press
+
+if __name__ == "__main__":
+	y = Code("unicode")
+	print(y)
